3e462d6a2a1f0514b8fe4b7452c2933fb2eabd03
mn <marvoosh@gmail.com>
2021-12-28 12:47:58 +0200

    :

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ document.querySelector(".again").addEventListener("click", function () {
   // document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
   score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";
